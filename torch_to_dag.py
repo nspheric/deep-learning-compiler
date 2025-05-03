a9bad21e8c5f7194059e574f6d3f00a2adc2e39e
Job Hernandez Lara <lararicciab@proton.me>
2025-05-03 16:40:15 -0700

    get input_tensors

--- torch_to_dag.py
+++ torch_to_dag.py
@@ -34,44 +34,51 @@ class Node:
 # is represented as a dag where the operations such as
 # relu and convolution are the nodes and the edges are
 # tensors, i.e., weights and bias
-def torch_to_dag(model):
+def torch_to_dag(model, input_tensor):
+    activations = {}
+    
+    for name, module in model.named_modules():
+        def hook_fn(mod, _input, _output, name=name):
+            activations[name] = _input[0]
+        if isinstance(module, (nn.Conv2d, nn.BatchNorm2d, nn.ReLU)):
+            module.register_forward_hook(hook_fn)
+            
+    model(input_tensor)
+
     traced_model = fx.symbolic_trace(model)
     nodes = list()
+
     for node in traced_model.graph.nodes:
         if node.op == 'call_module':
             layer_name = node.target
             layer = getattr(model, layer_name)
             if isinstance(layer, nn.Conv2d):
+                print({"convarg:": node.args})
+                input_tensor = activations.get(layer_name)
+                print({"input_tensor": input_tensor})
                 weights = layer.weight.data
                 bias = layer.bias.data
-                edges = list()
-                edges.append({"weights": weights})
-                edges.append({"bias": bias})
+                edges = [{"weights": weights}, {"bias": bias}]
                 nodes.append(Node("conv2d", edges))
-            if isinstance(layer, nn.BatchNorm2d):
-                edges = list()
-                weights = layer.weight.data
-                bias = layer.bias.data
-                edges.append({"weights": weights})
-                edges.append({"bias": bias})
+            elif isinstance(layer, nn.BatchNorm2d):
+                print({"batchnormargs": node.args})
+                input_tensor = activations.get(layer_name)
+                print({"input_tensor": input_tensor})
+                edges = [{"weights": layer.weight.data}, {"bias": layer.bias.data}]
                 nodes.append(Node("batchnorm2d", edges))
-            if isinstance(layer, nn.ReLU):
-                print("hello world")
-                edges = list()
-                edges.append({"input_tensor": node.args[0]})
+            elif isinstance(layer, nn.ReLU):
+                print({"reluargs": node.args})
+                input_tensor = activations.get(layer_name)
+                print({"input_tensor": input_tensor})
+                edges = [{"input_tensor": node.args[0]}]
                 nodes.append(Node("relu", edges))
-            
 
     dag = DAG(nodes)
     return dag
-                
-            
 
 vgg = VGGBlock()
-
-print(torch_to_dag(vgg))
-
-dag = torch_to_dag(vgg)
+input_tensor = torch.randn(1,3,224,224)
+dag = torch_to_dag(vgg, input_tensor)
 
 for node in dag.get_nodes():
     print(node.get_edges())
